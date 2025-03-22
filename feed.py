2a1721927a757e1d9e9e4df1a3533b0fac02d14f
Shridhar Agarwal <imagarwal.shri@gmail.com>
2025-03-23 02:24:01 +0530

    Generating basic rss with more content from yaml

--- feed.py
+++ feed.py
@@ -13,8 +13,37 @@ with open("feed.yaml", "r") as file:
         },
     )
 
+link_prefix = yaml_data["link"]
 channel_element = xml_tree.SubElement(rss_element, "channel")
 xml_tree.SubElement(channel_element, "title").text = yaml_data["title"]
+xml_tree.SubElement(channel_element, "format").text = yaml_data["format"]
+xml_tree.SubElement(channel_element, "subtitle").text = yaml_data["subtitle"]
+xml_tree.SubElement(channel_element, "itunes:author").text = yaml_data["author"]
+xml_tree.SubElement(channel_element, "description").text = yaml_data["description"]
+xml_tree.SubElement(
+    channel_element, "itunes:image", {"href": f"{link_prefix}{yaml_data['image']}"}
+)
+xml_tree.SubElement(channel_element, "language").text = yaml_data["language"]
+xml_tree.SubElement(channel_element, "link").text = link_prefix
+xml_tree.SubElement(channel_element, "itunes:category", {"text": yaml_data["category"]})
+
+for item in yaml_data["item"]:
+    item_element = xml_tree.SubElement(channel_element, "item")
+    xml_tree.SubElement(item_element, "title").text = item["title"]
+    xml_tree.SubElement(item_element, "description").text = item["description"]
+    xml_tree.SubElement(item_element, "itunes:author").text = yaml_data["author"]
+    xml_tree.SubElement(item_element, "itunes:duration").text = item["duration"]
+    xml_tree.SubElement(item_element, "pubDate").text = item["published"]
+    xml_tree.SubElement(
+        item_element,
+        "enclosure",
+        {
+            "length": item["length"],
+            "type": "audio/mpeg",
+            "url": f"{link_prefix}{item['file']}",
+        },
+    )
+
 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write("podcast.xml", encoding="UTF-8", xml_declaration=True)
